06e6f2789562cf93870fc618ed22152a26ff08c5
haojia666 <154444521@qq.com>
2020-08-08 15:41:30 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+  @import "~@/assets/style/helper.scss";
+  @import "~@/assets/style/reset.scss";
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #333;
+    font-family: $font-hei;
+    font-size: 16px;
+    line-height: 1.5;
+    background: #F5F5F5;
+  }
+  #app {
+    max-width: 500px;
+    margin: 0 auto;
+    border: 1px solid rgba(201, 130, 189, 0.281);
+  }
+</style>
